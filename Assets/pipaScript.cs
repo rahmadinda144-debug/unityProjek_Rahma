ba1cd6072e9819980bf16a28e4d6080309c91dcb
rahmadinda144-debug <rahmadinda144@gmail.com>
2026-01-19 23:18:41 +0800

    project start

---
+++ Assets/pipaScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class pipaScript : MonoBehaviour
+{
+    float nilaiRandom; // nilai desimal
+
+    void Start()
+    {
+        nilaiRandom = Random.Range(-1.32f, 0.3f);
+        transform.position = new Vector2(transform.position.x, nilaiRandom);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Vector2.MoveTowards(
+            transform.position,
+            Vector2.left * 100,
+            Time.deltaTime * 5
+        );
+    }
+}
